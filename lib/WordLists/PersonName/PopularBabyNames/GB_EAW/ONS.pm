15289abca6fdf148b2797a6d93741fe22f99f127
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-05-03 10:30:54 +0700

    Initial commit

---
+++ lib/WordLists/PersonName/PopularBabyNames/GB_EAW/ONS.pm
@@ -0,0 +1,16 @@
+package WordLists::PersonName::PopularBabyNames::GB_EAW::ONS;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Popular baby names in England and Wales (from ONS)
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+L<https://www.ons.gov.uk/>
